5531c1871e39ab291c47de13d302837d04d098e6
Asi Meir <45923568+prayingmantislab@users.noreply.github.com>
2022-08-13 19:55:04 +0300

    added style and content to questions screen

--- app/screens/question/question-screen.tsx
+++ app/screens/question/question-screen.tsx
@@ -1,18 +1,24 @@
 import React, { FC } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
+import { View, ViewStyle } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { NavigatorParamList } from "../../navigators"
 import { Screen, Text } from "../../components"
+// import { QuestionStore, QuestionStoreModel } from "../../models/question-store"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
-import { color } from "../../theme"
+import { color, spacing } from "../../theme"
 
 const ROOT: ViewStyle = {
-  backgroundColor: color.palette.black,
   flex: 1,
+  backgroundColor: color.palette.darkPlum,
+  paddingHorizontal: spacing[6],
 }
 
+const HEADER_CONTAINER: ViewStyle = {
+  marginTop: spacing[8],
+  marginBottom: spacing[3],
+}
 // STOP! READ ME FIRST!
 // To fix the TS error below, you'll need to add the following things in your navigation config:
 // - Add `question: undefined` to NavigatorParamList
@@ -22,15 +28,19 @@ const ROOT: ViewStyle = {
 
 // REMOVE ME! ⬇️ This TS ignore will not be necessary after you've added the correct navigator param type
 // @ts-ignore
-export const QuestionScreen: FC<StackScreenProps<NavigatorParamList, "question">> = observer(function QuestionScreen() {
-  // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
+export const QuestionScreen: FC<StackScreenProps<NavigatorParamList, "question">> = observer(
+  function QuestionScreen() {
+    // Pull in one of our MST stores
+    // const { someStore, anotherStore } = useStores()
 
-  // Pull in navigation via hook
-  // const navigation = useNavigation()
-  return (
-    <Screen style={ROOT} preset="scroll">
-      <Text preset="header" text="question" />
-    </Screen>
-  )
-})
+    // Pull in navigation via hook
+    // const navigation = useNavigation()
+    return (
+      <Screen style={ROOT} preset="scroll">
+        <View style={HEADER_CONTAINER}>
+          <Text preset="header" text="Questions" />
+        </View>
+      </Screen>
+    )
+  },
+)
